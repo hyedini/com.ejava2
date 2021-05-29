059cccb9e69bc6188c98e216f512d711c042a26a
hyedini <haejin475@gmail.com>
2021-05-29 21:53:29 +0900

    main url change

--- src/main/java/com/ejava/MainController.java
+++ src/main/java/com/ejava/MainController.java
@@ -1,12 +1,12 @@
 package com.ejava;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class MainController {
-    @GetMapping("/")
+    @RequestMapping(value = "/")
     public String main(){
-        return "wow!!! toy project";
+        return "main/index";
     }
 }
